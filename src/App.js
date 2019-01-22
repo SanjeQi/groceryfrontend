70daaa6045e5a3d1616c0c342317d8c9e5b39282
github username <github email address>
2019-01-22 10:32:25 +0000

    allow access from *

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
+  componentDidMount () {
+    this.getAllBeers()
+  }
+
+  getAllBeers = () => {
+    fetch('http://localhost:5000/api/v1/todos')
+      .then(resp => resp.json())
+      .then(data => console.log(data))
+  }
+
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
